71d8333600d5f2dcaae9051036ce53b0b63ccaac
na-bot-o <namusic7010@gmail.com>
2019-05-13 00:52:01 +0900

    Revert "first commit"
    This reverts commit da89d8ca1274d83426eec787e3834c69488725a7.

--- boot.sh
+++
@@ -1,2 +0,0 @@
-x86_64-w64-mingw32-gcc -Wall -Wextra -e efi_main -nostdinc -nostdlib -fno-builtin -Wl,--subsystem,10 -o fs/EFI/BOOT/BOOTX64.EFI: file.c efi.c common.c shell.c graphics.c gui.c main.c
-qemu-system-x86_64 -bios /usr/share/ovmf/OVMF.fd -hda fat:fs
--- gui.h
+++
@@ -1,8 +0,0 @@
-#ifndef _GUI_H_
-#define _GUI_H_
-
-void gui(void);
-int ls_gui(void);
-void cat_gui(unsigned short *file_name);
-
-#endif
